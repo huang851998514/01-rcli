f3860548b631cd63b1a1b2635d32758604098d11
onion <851998514@qq.com>
2024-04-29 15:56:08 +0800

    cli结构初步完成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(name = "rcli", version, author, about,long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Parser, Debug)]
+enum SubCommand {
+    #[command(about = "将csv文件转换为其他类型文件")]
+    Csv(CsvOptions),
+}
+
+#[derive(Parser, Debug)]
+struct CsvOptions {
+    #[arg(short, long)]
+    input: String,
+    #[arg(short, long, default_value = "output.json")]
+    output: String,
+    #[arg(short, long, default_value_t = ',')]
+    delimiter: char,
+    #[arg(long, default_value_t = true)]
+    header: bool,
+}
+
 fn main() {
-    println!("Hello, rust!");
+    let cli_parse = Cli::parse();
+    println!("{:?}", cli_parse);
 }
